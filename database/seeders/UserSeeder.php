ab744ef5b806270f67d61d7cc2eef7beddf507a0
salmanursholihah <salmanursholihah@gmail.com>
2026-08-27 11:14:40 +0700

    tambahan faker di seeder user (laptop)

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
